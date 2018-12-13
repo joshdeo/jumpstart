9794dd258c1bac199b29adfdfffe0c3b7a80776c
joshdeo <40075642+joshdeo@users.noreply.github.com>
2018-12-13 17:26:43 -0500

    site revised

--- main.js
+++ main.js
@@ -12,13 +12,28 @@ $(document).ready(function () {
 
 
     $(window).on('scroll', function () {
-        if ($(this).scrollTop() > 137) {
-            $('.navbar').fadeOut(500);
-        } else if ($(this).scrollTop() < 137) {
+        if ($(this).scrollTop() > 480) {
+            $('.navbar').fadeOut(200);
+        } else if ($(this).scrollTop() < 419) {
             $('.navbar').fadeIn(500);
         } else {
 
         };
         console.log($(this).scrollTop());
     });
+
+    /*
+    var x = $(window).scroll();
+    var y = $(this).scrollTop();
+    if (y - x < 50) {
+        var z = $('.navbar').css('height');
+        var nav = $('.navbar').animate({top: 150 + z}, 
+        150, 'easInOut');
+    } else if (x - y > 50) {
+        $nav.animate({top: $('.navbar').offset().top + 150}, 150);
+    } 
+    var x = y;
+    console.log(x);
+    console.log(y);
+    */
 });
