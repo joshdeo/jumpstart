59f89cafb62bb9b72de6d0373b89e793b1f1b560
joshdeo <40075642+joshdeo@users.noreply.github.com>
2018-12-12 19:44:41 -0500

    initial Slick init

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log('hi');
+
+$(document).ready(function () {
+    $('.carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false,
+        draggable: false,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+    });
+});
